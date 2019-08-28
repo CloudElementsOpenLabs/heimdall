dcdf39ed26b66ed99c4087a71c4f512b5c5a9635
mtrice <matt.trice@cloud-elements.com>
2019-08-28 17:36:28 -0600

    DELIVERY-1031 Handle token expired error I moved the try catch in uiAuthentication to wrap the entire function since util.findInRequest() throws the same error as util.parseToken() which was farther down in its own try catch. This allows the error response logic to consolidate to errorCatcher.js

--- server/errorCatcher.js
+++ server/errorCatcher.js
@@ -13,16 +13,19 @@ module.exports = (err, req, res, next) => {
         reason = "Bad Request. Array instead of Entity"
         res.status(400)
     }
+    else if (err.name === 'SequelizeEmptyResultError') {
+        reason = "Record with provided ID not found"
+        res.status(404)
+    }
+    else if (err.name === 'TokenExpiredError') {
+        reason = "Token expired"
+    }
     else if (err.error && err.error.message) {
         // CE error object includes: requestId, message, providermessage
         reason = err.error
     }
     else if (err.message) {
         reason = err.message
-    }
-    else if (err.name === 'SequelizeEmptyResultError') {
-        reason = "Record with provided ID not found"
-        res.status(404)
     }
     else {
         reason = "Unknown Error. Please contact support."
--- server/uiAuthentication.js
+++ server/uiAuthentication.js
@@ -22,78 +22,78 @@ const getKeyById = async id => {
 }
 
 module.exports = async (req, res, next) => {
-    // if this is the basic auth callback
-    if (req.body.instance) {
-        res.render('send-result', JSON.stringify({ id: req.body.instance.id, token: req.body.instance.token }))
-    }
-   
-    req.elementKey =  await pipe(util.findInRequest('elementKey'), getKeyById)(req)  
-    req.applicationId = util.findInRequest('applicationId', req)
-    req.instanceId = util.findInRequest('instanceId', req)
-    let userSecret = util.findInRequest('userSecret', req)
-
-    let token, hasQueryState;
-    if (req.elementKey && req.applicationId && userSecret) {
-        req.uniqueName = util.findInRequest('uName', req) || util.findInRequest('uniqueName', req)
-        req.application = await getApplication(req.applicationId)
+    try {
+        // if this is the basic auth callback
+        if (req.body.instance) {
+            res.render('send-result', JSON.stringify({ id: req.body.instance.id, token: req.body.instance.token }))
+        }
+    
+        req.elementKey =  await pipe(util.findInRequest('elementKey'), getKeyById)(req)  
+        req.applicationId = util.findInRequest('applicationId', req)
         req.instanceId = util.findInRequest('instanceId', req)
-        req.authData = {
-            userSecret : userSecret,
-            exp : util.findInRequest('exp', req),
-            iat : util.findInRequest('iat', req),
-            //already set, but need in authData too...
-            applicationId : req.applicationId,
-            orgSecret : req.application.orgSecret
-        }
+        let userSecret = util.findInRequest('userSecret', req)
 
-        next()
-        return
-    } else {
-        if (req.body.state || req.body.code) {
-            // restore all query parameter values from POST body so instance creation workflow works
-            Object.keys(req.body).forEach(function (paramKey) {
-                req.query[paramKey] = decodeURIComponent(req.body[paramKey])
-            });
-        }
-        if (req.query.state && req.query.state === 'oauth1' && req.cookies.state) {
-            hasQueryState = true;
-            token = req.cookies.state
+        let token, hasQueryState;
+        if (req.elementKey && req.applicationId && userSecret) {
+            req.uniqueName = util.findInRequest('uName', req) || util.findInRequest('uniqueName', req)
+            req.application = await getApplication(req.applicationId)
+            req.instanceId = util.findInRequest('instanceId', req)
+            req.authData = {
+                userSecret : userSecret,
+                exp : util.findInRequest('exp', req),
+                iat : util.findInRequest('iat', req),
+                //already set, but need in authData too...
+                applicationId : req.applicationId,
+                orgSecret : req.application.orgSecret
+            }
 
-        } else if (!token && req.query.code && req.cookies.state) {
-            hasQueryState = false
-            req.query.state = req.cookies.state
-            token = req.cookies.state
-        }
-    }
-    if (!token) {
-        res.status(401).send({ message: "Unauthorized, no userSecret or state found" })
-    } else if (req.query.state && !req.body.state && !req.body.code) {
-        // redirected from oauth -- need to render loading screen
-        res.render(`load-create`)
-
-    } else {
-        res.clearCookie('state');
-        if (req.body.state || req.body.code) {
-            delete req.body
-        }
-
-        try {
-            //process token from cookie
-            req.authData = util.parseToken(token)
-            req.elementKey = req.authData.elementKey
-            req.uniqueName = req.authData.uName
-            req.instanceId = req.authData.instanceId
-            req.application = await getApplication(req.authData.applicationId)
-            req.authData.orgSecret = req.application.orgSecret
-            if (req.instanceId === "undefined") { delete req.instanceId } 
-            delete req.authData.elementKey
-            delete req.authData.uName
-            req.token = token
             next()
-        } catch (err) {
-            res.status(401)
-            next(new Error(`The token is invalid, please re-launch the connector`))
+            return
+        } else {
+            if (req.body.state || req.body.code) {
+                // restore all query parameter values from POST body so instance creation workflow works
+                Object.keys(req.body).forEach(function (paramKey) {
+                    req.query[paramKey] = decodeURIComponent(req.body[paramKey])
+                });
+            }
+            if (req.query.state && req.query.state === 'oauth1' && req.cookies.state) {
+                hasQueryState = true;
+                token = req.cookies.state
+
+            } else if (!token && req.query.code && req.cookies.state) {
+                hasQueryState = false
+                req.query.state = req.cookies.state
+                token = req.cookies.state
+            }
         }
+        if (!token) {
+            res.status(401).send({ message: "Unauthorized, no userSecret or state found" })
+        } else if (req.query.state && !req.body.state && !req.body.code) {
+            // redirected from oauth -- need to render loading screen
+            res.render(`load-create`)
+
+        } else {
+            res.clearCookie('state');
+            if (req.body.state || req.body.code) {
+                delete req.body
+            }
+                //process token from cookie
+                req.authData = util.parseToken(token)
+                req.elementKey = req.authData.elementKey
+                req.uniqueName = req.authData.uName
+                req.instanceId = req.authData.instanceId
+                req.application = await getApplication(req.authData.applicationId)
+                req.authData.orgSecret = req.application.orgSecret
+                if (req.instanceId === "undefined") { delete req.instanceId } 
+                delete req.authData.elementKey
+                delete req.authData.uName
+                req.token = token
+                next()
+            }
+            
+    } catch (err) {
+        //handle in errorCatcher
+        next(err)
     }
 
 }
