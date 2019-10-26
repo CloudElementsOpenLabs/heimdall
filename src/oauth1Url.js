63667e6e1927f163d2fff72996882530fe1eddd9
mtrice <matt.trice@cloud-elements.com>
2019-10-25 19:09:04 -0600

    DR-123 send siteAddress key as parameter to oauth/url This heimdall config object: {   defaultValue: dev64365,   key: servicenow.subdomain,   oauthUrlParam: siteAddress }
    will now send:
    {
    servicenow.subdomain: dev64365
    siteAddress: dev64365
    ...
    }
    Previously only siteAddress was sent.

--- src/oauth1Url.js
+++ src/oauth1Url.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const {get} = require('./api')
-const {find, propEq, prop, pipe, curry} = require('ramda')
+const { get } = require('./api')
+const { find, propEq, prop, pipe, curry } = require('ramda')
 const util = require('./utils')
 
 const findAdditionalParams = curry((elementConfig, key) => pipe(find(propEq('oauthUrlParam', key)), prop('key'))(elementConfig))
@@ -12,12 +12,12 @@ module.exports = async (req, configuration, config) => {
     const state = util.createToken({
         userSecret: req.authData.userSecret,
         applicationId: req.authData.applicationId,
-        elementKey: req.elementKey,  
+        elementKey: req.elementKey,
         configuration: configuration,
         uName: req.uniqueName,
         instanceId: req.instanceId
     })
-    const oauth1Token = await get(`elements/${req.elementKey}/oauth/token`, 
+    const oauth1Token = await get(`elements/${req.elementKey}/oauth/token`,
         {
             apiKey: getElementConfig('oauth.api.key') ? getElementConfig('oauth.api.key') : configuration[findOauthParams('apiKey')],
             apiSecret: getElementConfig('oauth.api.secret') ? getElementConfig('oauth.api.secret') : configuration[findOauthParams('apiSecret')],
@@ -27,17 +27,19 @@ module.exports = async (req, configuration, config) => {
             'authentication.type': getElementConfig('authentication.type'),
         }, req.authData)
 
+    let payload = {
+        apiKey: getElementConfig('oauth.api.key') ? getElementConfig('oauth.api.key') : configuration[findOauthParams('apiKey')],
+        apiSecret: getElementConfig('oauth.api.secret') ? getElementConfig('oauth.api.secret') : configuration[findOauthParams('apiSecret')],
+        callbackUrl: getElementConfig('oauth.callback.url') + `?state=oauth1`,
+        scope: getElementConfig('oauth.scope'),
+        'authentication.type': getElementConfig('authentication.type'),
+        requestToken: oauth1Token.token
+    }
+    // send both and let CE decide
+    const subdomain = findOauthParams('siteAddress')
+    payload.siteAddress = configuration[subdomain]
+    payload[subdomain] = configuration[subdomain]
 
-    const url = (await get(`elements/${req.elementKey}/oauth/url`,
-        {
-            apiKey: getElementConfig('oauth.api.key') ? getElementConfig('oauth.api.key') : configuration[findOauthParams('apiKey')],
-            apiSecret: getElementConfig('oauth.api.secret') ? getElementConfig('oauth.api.secret') : configuration[findOauthParams('apiSecret')],
-            callbackUrl: getElementConfig('oauth.callback.url') + `?state=oauth1`,
-            siteAddress: configuration[findOauthParams('siteAddress')],
-            scope: getElementConfig('oauth.scope'),
-            'authentication.type': getElementConfig('authentication.type'),
-            requestToken: oauth1Token.token
-        }, req.authData)).oauthUrl
-
-    return {url, secret: oauth1Token.secret, state}
+    const url = (await get(`elements/${req.elementKey}/oauth/url`, payload, req.authData)).oauthUrl
+    return { url, secret: oauth1Token.secret, state }
 }
--- src/oauthUrl.js
+++ src/oauthUrl.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const {get} = require('./api')
-const {find, propEq, prop, pipe, curry} = require('ramda')
+const { get } = require('./api')
+const { find, propEq, prop, pipe, curry } = require('ramda')
 const util = require('./utils')
 
 const findAdditionalParams = curry((elementConfig, key) => pipe(find(propEq('oauthUrlParam', key)), prop('key'))(elementConfig))
@@ -12,20 +12,26 @@ module.exports = async (req, configuration, config) => {
     const state = util.createToken({
         userSecret: req.authData.userSecret,
         applicationId: req.authData.applicationId,
-        elementKey: req.elementKey, 
+        elementKey: req.elementKey,
         configuration: configuration,
         uName: req.uniqueName,
         instanceId: req.instanceId
     })
 
-    let url = (await get(`elements/${req.elementKey}/oauth/url`,
-        {
-            apiKey: getElementConfig('oauth.api.key') ? getElementConfig('oauth.api.key') : configuration[findOauthParams('apiKey')],
-            apiSecret: getElementConfig('oauth.api.secret') ? getElementConfig('oauth.api.secret') : configuration[findOauthParams('apiSecret')],
-            callbackUrl: getElementConfig('oauth.callback.url') ? getElementConfig('oauth.callback.url') : configuration[findOauthParams('callbackUrl')],
-            siteAddress: configuration[findOauthParams('siteAddress')],
-            scope: getElementConfig('oauth.scope'),
-            'authentication.type': getElementConfig('authentication.type'),
-        }, req.authData)).oauthUrl
+    let payload = {
+        apiKey: getElementConfig('oauth.api.key') ? getElementConfig('oauth.api.key') : configuration[findOauthParams('apiKey')],
+        apiSecret: getElementConfig('oauth.api.secret') ? getElementConfig('oauth.api.secret') : configuration[findOauthParams('apiSecret')],
+        callbackUrl: getElementConfig('oauth.callback.url') ? getElementConfig('oauth.callback.url') : configuration[findOauthParams('callbackUrl')],
+        scope: getElementConfig('oauth.scope'),
+        'authentication.type': getElementConfig('authentication.type')
+    }
+    // some elements use siteAddress normalization (zendesk, quickbooks, salesforce, desk, dynamicscrmadfs)
+    // but some don't (servicenowoauth)
+    // send both and let CE decide
+    const subdomain = findOauthParams('siteAddress')
+    payload.siteAddress = configuration[subdomain]
+    payload[subdomain] = configuration[subdomain]
+
+    const url = (await get(`elements/${req.elementKey}/oauth/url`, payload, req.authData)).oauthUrl
     return { url, state }
 }
